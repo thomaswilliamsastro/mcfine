782a11fa24353f71481a835109eebf46e2e3e9c1
Thomas Williams <thomas.g.williams94@gmail.com>
2026-02-11 15:37:52 +0000

    Bump to v0.4
    - Bump to v0.4

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2025, Thomas Williams'
 author = 'Thomas Williams'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3'
+release = '0.4'
 
 
 # -- General configuration ---------------------------------------------------
